4842c567b018aab960524d391f9c3dd674581434
Sumanyu Sharma <sumanyukodan301@gmail.com>
2023-08-22 10:25:13 +0530

    5th

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,43 +1,16 @@
-// design a structure to represent customer and create an array of structures for nearby 
-// stores develop a c program that utilizes the pythagorian theoram calculate distances 
-// the closest store for the customers to place their orders
-
 #include <stdio.h>
-#include <math.h>
-#include <limits.h>
+# define pi 3.14
 
-struct distance {
-    char name[20];
-    int x;
-    int y;
-};
-
-int shortest_distance(struct distance arr[], int a, int b) {
-    int min_distance = INT_MAX;
-    int x=0;
-    for (int i = 0; i < 4; i++) {
-        int distance = sqrt((arr[i].x - a) * (arr[i].x - a) + (arr[i].y - b) * (arr[i].y - b));
-        if (distance < min_distance) {
-            min_distance = distance;
-            x=i;
-        }
-    }
-    printf("NAME of the hotel = %s",arr[x].name);
-    return min_distance;
+int main()
+{
+inline float area(float *r ,float *h,float *are , float *volume)
+{
+    *are = 2*pi*(*r)*(*r+*h);
+    *volume = pi*(*r)*(*r)*(*h);
 }
-
-int main() {
-    struct distance arr[4];
-    arr[0] = (struct distance){"a", 2, 3};
-    arr[1] = (struct distance){"b", 7,   9};
-    arr[2] = (struct distance){"c", 13, 14};
-    arr[3] = (struct distance){"d", 93, 17};
-    int a ;
-    int b ;
-    printf("the coordinates of customer = ");
-    scanf("%d %d",&a,&b);
-    int closest_dist = shortest_distance(arr, a, b);
-    printf("\n");
-    printf("%d", closest_dist);
+    float a,b,are,volume;
+    scanf("%f %f",&a,&b);
+    area(&a,&b,&are,&volume);
+    printf("%f \n %f",are,volume);
     return 0;
 }
