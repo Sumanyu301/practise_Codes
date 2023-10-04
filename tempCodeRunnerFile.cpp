0f3be451dcf3d19c8974a1c49da4905f9ca8505e
Sumanyu Sharma <sumanyukodan301@gmail.com>
2023-10-04 20:11:45 +0530

    4-10-23

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,23 +1,29 @@
-//find sum of array using recursion
+//givrn an arrat and sum find if we can make sum = k using elements of array , elementd can be in any order.
 #include <iostream>
 using namespace std;
 
-int sumArray(int arr[], int n) {
-    if (n <= 0) {
-        return 0;
-    } else {
-        return arr[n - 1] + sumArray(arr, n - 1);
+bool canmakesum(int arr[], int n, int k) {
+    if (k == 0) {
+        return true; 
     }
+    if (n <= 0) {
+        return false; 
+    }
+    return canmakesum(arr, n - 1, k - arr[n - 1]) || canmakesum(arr, n - 1, k);
 }
 
 int main() {
-    int n;
+    int n, k;
     cin >> n;
     int arr[n];
+        cin >> k;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    int result = sumArray(arr, n);
-    cout <<result << endl;
+    if (canmakesum(arr, n, k)) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
     return 0;
 }
