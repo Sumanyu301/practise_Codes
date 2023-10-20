73da509db2a583b476dfe057e3f6d1c5db65b34c
Sumanyu Sharma <sumanyukodan301@gmail.com>
2023-10-20 20:00:04 +0530

    20-10-23

--- code_Forces_PRactice/code_forces_131-A.cpp
+++ code_Forces_PRactice/code_forces_131-A.cpp
@@ -1,24 +1,37 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    string str;
-    cin >> str;
-    for (int i = 0; str[i] = '\0'; i++)
-    {
-        if (i == 0)
-        {
-            if ('a' <= str[i] && str[i] <= 'z')
-            {
-                str[i] -= 32;
-            }
-        }
-        else
-            {
-                tolower(str[i]);
-            }
-    }
-    cout << str;
-    return 0;
+string s;
+int x ;
+
+int main(){
+	
+	cin >> s;
+	int kq = true;
+	x = int(s[0]);
+	
+	if (x >= 65 && x <= 90){
+		for (int i = 1; i < s.size() ; i++)
+			if (int(s[i] >= 97 && int(s[i]) <= 122))
+				kq = false;
+		if (kq == true){
+			for (int i = 0; i < s.size() ; i++)
+				s[i] = char(int(s[i]) + 32);
+		}
+	}
+	
+	if (x >= 97 && x <= 122){
+		for (int i = 1; i < s.size() ; i++)
+			if (int(s[i] >= 97 && int(s[i]) <= 122))
+				kq = false;
+		if (kq == true){
+			s[0] = char(int(s[0])-32);
+			for (int i = 1; i < s.size() ; i++)
+				s[i] = char(int(s[i]) + 32);
+		}
+	}
+	
+	cout << s;
+	
+	return 0;
 }
