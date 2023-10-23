11a11d1fb94723b7662e3226b8f968c332c812e6
Sumanyu Sharma <sumanyukodan301@gmail.com>
2023-10-23 22:32:26 +0530

    23-10-23

---
+++ code_Forces_PRactice/code_forces_43-A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int max_count = 0;
+    int k;
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+                if (count > max_count)
+                {
+                    max_count = count;
+                    k = i;
+                }
+            }
+        }
+    }
+    cout << arr[k];
+    return 0;
+}
