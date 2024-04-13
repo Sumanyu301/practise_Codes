73e4adbb94f41e4cb402f5967e0294fcb0db034a
Sumanyu Sharma <sumanyukodan301@gmail.com>
2024-04-13 08:46:41 +0530

    adding questions

--- code_Forces_PRactice/code_forces_14055-B.cpp
+++ code_Forces_PRactice/code_forces_14055-B.cpp
@@ -27,35 +27,135 @@
 // ⣿⣿⣿⣿⣿⣿⣿⠏⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⢇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 // ⣿⣿⣿⣿⣿⣿⠄ ⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 #include <bits/stdc++.h>
-#include<sstream>
 using namespace std;
 #define endl '\n'
 #define int long long
 const int MOD = 1e9 + 7;
 const int INF = LLONG_MAX >> 1;
-signed main() {
-    int n;
-    cin>>n;
-    string s[n];
-    for(int i=0;i<n;i++)
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--)
     {
-        cin>>s[i];
-    }
-    string s2;
-    cin>>s2;
-    int flag = 0;
-    for(int i=0;i<n;i++)
-    {
-        if(s[i]==s2)
+        int n, k, m;
+        cin >> n >> m >> k;
+        int arr[n];
+        int brr[m];
+        for (int i = 0; i < n; i++)
         {
-            cout<<i;
-            flag = 1;
-            break;
+            cin >> arr[i];
         }
-    }
-    if(flag==0)
-    {
-        cout<<"-1";
+        for (int i = 0; i < m; i++)
+        {
+            cin >> brr[i];
+        }
+        sort(arr, arr + n);
+        sort(brr, brr + m);
+        int freq[k + 1] = {0};
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] <= k)
+            {
+                if (arr[i] != arr[i - 1])
+                    freq[arr[i]] += 1;
+            }
+        }
+        for (int i = 0; i < m; i++)
+        {
+            if (brr[i] <= k)
+            {
+                if (brr[i] != brr[i - 1])
+                    freq[brr[i]] += 2;
+            }
+        }
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        for (int i = 0; i <= k; i++)
+        {
+            if (freq[i] == 1)
+            {
+                count1++;
+            }
+            else if (freq[i] == 2)
+            {
+                count2++;
+            }
+            else if (freq[i] == 3)
+            {
+                count3++;
+            }
+        }
+        int x = count1 - k / 2;
+        int y = count2 - k / 2;
+        if (x > 0 || y > 0)
+        {
+            cout << "NO" << endl;
+            continue;
+        }
+        if(x==0 && y==0)
+        {
+            cout<<"YES"<<endl;
+            continue;
+        }
+        if(x==0)
+        {
+            if(count3 + y >= 0)
+            {
+                cout<<"YES"<<endl;
+                continue;
+            }
+            else
+            {
+                cout<<"NO"<<endl;
+                continue;
+            }
+        }
+        if(y==0)
+        {
+            if(count3 + x >= 0)
+            {
+                cout<<"YES"<<endl;
+                continue;
+            }
+            else
+            {
+                cout<<"NO"<<endl;
+                continue;
+            }
+        }
+        if ((count3 + x) >= 0)
+        {
+            count3 += x;
+            if (count3 + y >= 0)
+            {
+                cout << "YES" << endl;
+                continue;
+            }
+            else
+            {
+                cout << "NO" << endl;
+                continue;
+            }
+        }
+        if ((count3 + y) >= 0)
+        {
+            count3 += y;
+            
+            if (count3 + x >= 0)
+            {
+                cout << "YES" << endl;
+                continue;
+            }
+            else
+            {
+                cout << "NO" << endl;
+                continue;
+            }
+        }
     }
     return 0;
 }
